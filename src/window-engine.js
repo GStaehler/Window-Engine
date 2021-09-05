fc2792133dac09304512ecb18cca2d86096983bc
Matteo Gheri <57362031+matteo-gheri-pocketworks@users.noreply.github.com>
2021-09-05 09:23:30 +0100

    adds showWindown on body click

--- src/window-engine.js
+++ src/window-engine.js
@@ -1,145 +1,158 @@
 /* Window Engine - MIT License - Copyright (c) 2019 Gauthier Staehler */
 
+const config = {
+  showOnClick: false,
+};
+
 const metaTag = document.createElement("meta");
 metaTag.name = "viewport";
 metaTag.content = "user-scalable=0";
 document.getElementsByTagName("head")[0].appendChild(metaTag);
 
-const lastWindow = document.getElementsByClassName("windowGroup")[0].lastElementChild.id.substring(6);
+const lastWindow = document
+  .getElementsByClassName("windowGroup")[0]
+  .lastElementChild.id.substring(6);
 const active = document.getElementsByClassName("window");
 
 for (let i = 1; i <= lastWindow; i++) {
-    createWindow(i);
+  createWindow(i);
 }
 
 function createWindow(id) {
-    let windowID = document.getElementById("window" + id);
-    let headerID = windowID.firstElementChild;
-    headerID.id = "window" + id + "header";
+  let windowID = document.getElementById("window" + id);
+  let headerID = windowID.firstElementChild;
+  headerID.id = "window" + id + "header";
 
-    let createCloseButton = document.createElement("b");
-    createCloseButton.id = "closeButton" + id;
-    createCloseButton.innerHTML = "×";
-    document.getElementById("window" + id + "header").appendChild(createCloseButton);
+  windowID.setAttribute("data-showonclick", config.showOnClick);
 
-    document.getElementById("closeButton" + id).onclick = function () {
-        fadeOut(windowID);
-    };
-    document.getElementById("button" + id).onclick = function () {
-        if (windowID.style.display === "initial") {
-			activeWindow(windowID);
-        } else {
-            windowID.style = "position: absolute;";
-            windowID.style = "top: 80px;";
-            fadeIn(windowID);
-        }
-    };
-    dragElement(windowID);
+  let createCloseButton = document.createElement("b");
+  createCloseButton.id = "closeButton" + id;
+  createCloseButton.innerHTML = "×";
+  document
+    .getElementById("window" + id + "header")
+    .appendChild(createCloseButton);
+
+  document.getElementById("closeButton" + id).onclick = function () {
+    fadeOut(windowID);
+  };
+  document.getElementById("button" + id).onclick = function () {
+    if (windowID.style.display === "initial") {
+      activeWindow(windowID);
+    } else {
+      windowID.style = "position: absolute;";
+      windowID.style = "top: 80px;";
+      fadeIn(windowID);
+    }
+  };
+  dragElement(windowID);
 }
 
 function dragElement(elmnt) {
-    var pos1 = 0,
-        pos2 = 0,
-        pos3 = 0,
-        pos4 = 0;
+  var pos1 = 0,
+    pos2 = 0,
+    pos3 = 0,
+    pos4 = 0;
+  if ("ontouchstart" in document.documentElement) {
+    var pos1touch = 0,
+      pos2touch = 0,
+      pos3touch = 0,
+      pos4touch = 0;
+  }
+  if (document.getElementById(elmnt.id + "header")) {
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    document.getElementById(elmnt.id + "header").ontouchstart = dragMouseDown;
+  }
+  console.log(elmnt.dataset.showonclick);
+  if (elmnt.dataset.showonclick == "true")
+    elmnt.onmousedown = () => activeWindow(elmnt);
+
+  function dragMouseDown(e) {
+    if (!"ontouchstart" in document.documentElement) {
+      e.preventDefault();
+    }
+    pos3 = e.clientX;
+    pos4 = e.clientY;
     if ("ontouchstart" in document.documentElement) {
-        var pos1touch = 0,
-            pos2touch = 0,
-            pos3touch = 0,
-            pos4touch = 0;
+      try {
+        pos3touch = e.touches[0].clientX;
+        pos4touch = e.touches[0].clientY;
+      } catch (error) {}
     }
-    if (document.getElementById(elmnt.id + "header")) {
-        document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
-        document.getElementById(elmnt.id + "header").ontouchstart = dragMouseDown;
-    }
+    document.onmouseup = closeDragElement;
+    document.onmousemove = elementDrag;
+    document.ontouchend = closeDragElement;
+    document.ontouchmove = elementDrag;
+    activeWindow(document.getElementById(elmnt.id));
+  }
 
-    function dragMouseDown(e) {
-        if (!"ontouchstart" in document.documentElement) {
-            e.preventDefault();
-        }
-        pos3 = e.clientX;
-        pos4 = e.clientY;
-        if ("ontouchstart" in document.documentElement) {
-            try {
-                pos3touch = e.touches[0].clientX;
-                pos4touch = e.touches[0].clientY;
-            } catch(error) {}
-        }
-        document.onmouseup = closeDragElement;
-        document.onmousemove = elementDrag;
-        document.ontouchend = closeDragElement;
-        document.ontouchmove = elementDrag;
-        activeWindow(document.getElementById(elmnt.id));
+  function elementDrag(e) {
+    e.preventDefault();
+    if ("ontouchstart" in document.documentElement) {
+      pos1touch = pos3touch - e.touches[0].clientX;
+      pos2touch = pos4touch - e.touches[0].clientY;
+      pos3touch = e.touches[0].clientX;
+      pos4touch = e.touches[0].clientY;
+      elmnt.style.top = elmnt.offsetTop - pos2touch + "px";
+      elmnt.style.left = elmnt.offsetLeft - pos1touch + "px";
+    } else {
+      pos1 = pos3 - e.clientX;
+      pos2 = pos4 - e.clientY;
+      pos3 = e.clientX;
+      pos4 = e.clientY;
+      elmnt.style.top = elmnt.offsetTop - pos2 + "px";
+      elmnt.style.left = elmnt.offsetLeft - pos1 + "px";
     }
+  }
 
-    function elementDrag(e) {
-        e.preventDefault();
-        if ("ontouchstart" in document.documentElement) {
-            pos1touch = pos3touch - e.touches[0].clientX;
-            pos2touch = pos4touch - e.touches[0].clientY;
-            pos3touch = e.touches[0].clientX;
-            pos4touch = e.touches[0].clientY;
-            elmnt.style.top = (elmnt.offsetTop - pos2touch) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1touch) + "px";
-        } else {
-            pos1 = pos3 - e.clientX;
-            pos2 = pos4 - e.clientY;
-            pos3 = e.clientX;
-            pos4 = e.clientY;
-            elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
-            elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
-        }
-    }
-
-    function closeDragElement() {
-        document.onmouseup = null;
-        document.onmousemove = null;
-        document.ontouchend = null;
-        document.ontouchmove = null;
-    }
+  function closeDragElement() {
+    document.onmouseup = null;
+    document.onmousemove = null;
+    document.ontouchend = null;
+    document.ontouchmove = null;
+  }
 }
 
 function fadeIn(elmnt) {
-    elmnt.style.opacity = 0;
-    elmnt.style.display = "initial";
-    if (elmnt.classList.contains("fade")) {
-        var opacity = 0;
-        var timer = setInterval(function () {
-            opacity += 30 / 70;
-            if (opacity >= 1) {
-                clearInterval(timer);
-                opacity = 0.9;
-            }
-            elmnt.style.opacity = opacity;
-            activeWindow(elmnt);
-        }, 50);
-    } else {
-        elmnt.style.opacity = "0.9";
-        activeWindow(elmnt);
-    }
+  elmnt.style.opacity = 0;
+  elmnt.style.display = "initial";
+  if (elmnt.classList.contains("fade")) {
+    var opacity = 0;
+    var timer = setInterval(function () {
+      opacity += 30 / 70;
+      if (opacity >= 1) {
+        clearInterval(timer);
+        opacity = 0.9;
+      }
+      elmnt.style.opacity = opacity;
+      activeWindow(elmnt);
+    }, 50);
+  } else {
+    elmnt.style.opacity = "0.9";
+    activeWindow(elmnt);
+  }
 }
 
 function fadeOut(elmnt) {
-    if (elmnt.classList.contains("fade")) {
-        var opacity = 1;
-        var timer = setInterval(function () {
-            opacity -= 30 / 70;
-            if (opacity <= 0) {
-                clearInterval(timer);
-                opacity = 0;
-                elmnt.style.display = "none";
-            }
-            elmnt.style.opacity = opacity;
-        }, 50);
-    } else {
+  if (elmnt.classList.contains("fade")) {
+    var opacity = 1;
+    var timer = setInterval(function () {
+      opacity -= 30 / 70;
+      if (opacity <= 0) {
+        clearInterval(timer);
+        opacity = 0;
         elmnt.style.display = "none";
-        activeWindow(elmnt);
-    }
+      }
+      elmnt.style.opacity = opacity;
+    }, 50);
+  } else {
+    elmnt.style.display = "none";
+    activeWindow(elmnt);
+  }
 }
 
 function activeWindow(elmnt) {
-    for (let i = active.length - 1; i > -1; i--) {
-        active[i].classList.remove("windowActive");
-        elmnt.className += " windowActive";
-    }
+  for (let i = active.length - 1; i > -1; i--) {
+    active[i].classList.remove("windowActive");
+    elmnt.className += " windowActive";
+  }
 }
